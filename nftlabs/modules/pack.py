72209fa69ec16d0fb7c23a726598a2583c53b68b
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-10-19 13:33:21 -0700

    Filled in some pack methods:

--- nftlabs/modules/pack.py
+++ nftlabs/modules/pack.py
@@ -32,28 +32,37 @@ class PackModule(BaseModule):
         pass
 
     def balance_of(self, address: str, token_id: int) -> int:
-        pass
+        return self.__abi_module.balance_of.call(address, token_id)
 
-    def balance(self, token_id) -> int:
-        pass
+    def balance(self, token_id: int) -> int:
+        return self.__abi_module.balance_of.call(self.get_signer_address(), token_id)
 
     def is_approved(self, address: str, operator: str) -> bool:
-        pass
+        return self.__abi_module.is_approved_for_all.call(address, operator)
 
     def set_approval(self, operator: str, approved: bool):
-        pass
+        return self.execute_tx(self.__abi_module.set_approval_for_all.build_transaction(
+            operator, approved, self.get_transact_opts()
+        ))
 
     def transfer(self, to_address: str, token_id: int, amount: int):
-        pass
+        return self.execute_tx(self.__abi_module.safe_transfer_from.build_transaction(
+            self.get_signer_address(), to_address, token_id, amount, "", self.get_transact_opts(),
+        ))
 
     def create(self, arg: CreatePackArg) -> PackMetadata:
         pass
 
     def transfer_from(self, from_address: str, to_address: str, args: AssetAmountPair):
-        pass
+        return self.execute_tx(self.__abi_module.safe_transfer_from.build_transaction(
+            from_address, to_address, args.token_id, args.amount, "", self.get_transact_opts(),
+        ))
 
     def transfer_batch_from(self, from_address: str, to_address: str, args: List[AssetAmountPair]):
-        pass
+        ids, amounts = [i.token_id for i in args], [i.amount for i in args]
+        return self.execute_tx(self.__abi_module.safe_batch_transfer_from.build_transaction(
+            from_address, to_address, ids, amounts, "", self.get_transact_opts(),
+        ))
 
     def get_link_balance(self) -> CurrencyValue:
         pass
@@ -65,10 +74,14 @@ class PackModule(BaseModule):
         pass
 
     def set_royalty_bps(self, amount: int):
-        pass
+        return self.execute_tx(self.__abi_module.set_royalty_bps.build_transaction(
+            amount, self.get_transact_opts()
+        ))
 
-    def set_restricted_transfer(self, restricted: bool = False):
-        pass
+    def set_restricted_transfer(self, restricted: bool = True):
+        return self.execute_tx(self.__abi_module.set_restricted_transfer.build_transaction(
+            restricted, self.get_transact_opts()
+        ))
 
     def grant_role(self, role: Role, address: str):
         role_hash = role.get_hash()
