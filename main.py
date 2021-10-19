973784c5f7e4e87241c093c52932b76965060d46
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-10-19 12:33:05 -0700

    Added more examples to main.py

--- main.py
+++ main.py
@@ -7,22 +7,23 @@ from nftlabs.modules.nft_types import MintArg
 from nftlabs.types import Role
 
 options = nftlabs.options.SdkOptions()
-sdk = NftlabsSdk(options, "https://rpc-mumbai.maticvigil.com")
+# sdk = NftlabsSdk(options, "https://rpc-mumbai.maticvigil.com")
+sdk = NftlabsSdk(options, "https://polygon-rpc.com")
 sdk.set_private_key(os.getenv("PKEY"))
+#
+# currency_module = sdk.get_currency_module("0xd75807e8D122A4DDF3bCCBB67fC33D8d78955726")
+#
+# pprint(currency_module.get_value(1))
+# pprint(currency_module.get_value(100000000000000000))
+# pprint(currency_module.get_value(1000000000000000000))
+# pprint(currency_module.get_value(10000000000000000000))
+#
+# pprint(currency_module.total_supply())
+# pprint(currency_module.get())
+# pprint(currency_module.get_all_role_members())
+#
 
-currency_module = sdk.get_currency_module("0xd75807e8D122A4DDF3bCCBB67fC33D8d78955726")
-
-pprint(currency_module.get_value(1))
-pprint(currency_module.get_value(100000000000000000))
-pprint(currency_module.get_value(1000000000000000000))
-pprint(currency_module.get_value(10000000000000000000))
-
-pprint(currency_module.total_supply())
-pprint(currency_module.get())
-pprint(currency_module.get_all_role_members())
-
-
-nft_module = sdk.get_nft_module("0xbDfF8fb43688fB4D2184DF8029A7238ac1413A24")
+nft_module = sdk.get_nft_module("0x718526F74cD27bAc8ae256D2B94dB4a8c640C283")
 print(nft_module.total_supply())
 
 # minted_nft = nft_module.mint(arg=MintArg(name="Test 123", description="Some description"))
