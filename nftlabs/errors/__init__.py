5e20b1b43b1c8ee61b0ce5b94509e11d1b3f26f1
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-10-21 12:59:08 -0700

    Added asset not found error

--- nftlabs/errors/__init__.py
+++ nftlabs/errors/__init__.py
@@ -1,4 +1,11 @@
+from typing import Any, Optional
+
 
 class NoSignerException(Exception):
     def __init__(self):
         super().__init__("No signer (private key) supplied to SDK")
+
+
+class AssetNotFoundException(Exception):
+    def __init__(self, identifier: Optional[Any] = None):
+        super().__init__(f"Asset with ID '{identifier}' not found" if identifier is not None else "Asset not found")
