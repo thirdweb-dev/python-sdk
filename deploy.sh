d13d1009591cc90b88283bb2c3e5b415f19f9849
Ayush Pathak <62694274+ayshptk@users.noreply.github.com>
2021-10-25 13:49:06 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 # DO NOT RUN LOCALLY
-pip install pydoctor && pydoctor --make-html --html-output=. nftlabs/modules && rm main.py && rm requirements.txt && rm setup.py && rm README.md  && rm -rf nftlabs && rm -- "$0"
+pip install pydoctor && pydoctor --make-html --html-output=docs nftlabs/modules 
