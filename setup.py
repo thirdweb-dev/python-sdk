f1e86c84928f8e693ad782c94b381328c8587a4f
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-11-03 16:26:39 -0700

    Updated version to 0.2.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="nftlabs-sdk",
-    version="0.2.0",
+    version="0.2.1",
     description="Official Nftlabs sdk",
     long_description=README,
     long_description_content_type="text/markdown",
