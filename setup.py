eb90910a2b515a784b7a16ab3cedf18d59ffd824
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-11-09 14:23:44 -0800

    Update package version, updated requirements to include the new packages

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if "PACKAGE_NAME" in environ:
 
 setup(
     name=package_name,
-    version="0.3.0",
+    version="0.3.1",
     description="Official Thirdweb sdk",
     long_description=README,
     long_description_content_type="text/markdown",
