731dbed8e129ebcf021bc3d24d609571b453fe36
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-11-09 14:03:12 -0800

    Fix mirrored module import

--- nftlabs/__init__.py
+++ nftlabs/__init__.py
@@ -1,9 +1,3 @@
 import sys
 
-# make sure bar is in sys.modules
-import thirdweb
-# link this module to bar
-sys.modules[__name__] = sys.modules['thirdweb']
-
-# Or simply
 sys.modules[__name__] = __import__('thirdweb')
