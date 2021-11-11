4f26135db1c20ddeaa973374a70321c5e18f5766
Ibrahim Ahmed <abeahmed2@gmail.com>
2021-11-11 12:58:48 -0800

    Move to ipfs.io gateway and set strict fetch timeout

--- thirdweb/options/sdk_options.py
+++ thirdweb/options/sdk_options.py
@@ -3,7 +3,7 @@ import dataclasses
 
 @dataclasses.dataclass
 class SdkOptions:
-    ipfs_gateway_url: str = "https://cloudflare-ipfs.com/ipfs/"
+    ipfs_gateway_url: str = "https://ipfs.io/ipfs/"
     registry_contract_address: str = ""
     max_gas_price_in_gwei: int = 100
     gas_speed: str = "fastest"
