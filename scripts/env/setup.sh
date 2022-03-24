654795050207e08f88b064d5a483b13764cbba0e
adam-maj <mr.adam.maj@gmail.com>
2022-03-24 12:59:36 -0400

    Local testing environment working! I think?

---
+++ scripts/env/setup.sh
@@ -0,0 +1,10 @@
+# Make sure your environment is deactivated before running this script.
+
+rm -rf .venv
+
+# Make sure to select .venv as the VSCode project directory
+poetry shell
+poetry install
+
+# Make sure ganache is installed globally as it is a dependency of brownie
+poetry run yarn global add ganache
