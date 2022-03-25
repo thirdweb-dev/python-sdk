1ef37e2b7f36eba64c1680a6d70f72ba8359e536
adam-maj <mr.adam.maj@gmail.com>
2022-03-25 15:38:17 -0400

    Add NFT Collection interface

--- thirdweb/common/nft.py
+++ thirdweb/common/nft.py
@@ -1,4 +1,5 @@
-from typing import cast
+from typing import List, Union, cast
+from thirdweb.common.error import UploadException
 from thirdweb.core.classes.ipfs_storage import IpfsStorage
 from thirdweb.types.nft import NFTMetadata
 
@@ -18,3 +19,34 @@ def fetch_token_metadata(
         metadata.get("background_color"),
         metadata.get("properties"),
     )
+
+
+def upload_or_extract_uri(
+    metadata: Union[NFTMetadata, str], storage: IpfsStorage
+) -> str:
+    if isinstance(metadata, str):
+        return metadata
+    else:
+        return storage.upload_metadata(metadata)
+
+
+def is_uri_list(metadatas: List[Union[NFTMetadata, str]]) -> bool:
+    return all(isinstance(metadata, str) for metadata in metadatas)
+
+
+def is_metadata_list(metadatas: List[Union[NFTMetadata, str]]) -> bool:
+    return all(isinstance(metadata, NFTMetadata) for metadata in metadatas)
+
+
+def upload_or_extract_uris(
+    metadatas: List[Union[NFTMetadata, str]], storage: IpfsStorage
+) -> List[str]:
+    if is_uri_list(metadatas):
+        return cast(List[str], metadatas)
+    elif is_metadata_list(metadatas):
+        uri_with_metadata = storage.upload_metadata_batch(metadatas)
+        return uri_with_metadata.metadata_uris
+    else:
+        raise UploadException(
+            "Metadata must be either a list of strings or a list of NFTMetadata"
+        )
--- thirdweb/contracts/nft_collection.py
+++ thirdweb/contracts/nft_collection.py
@@ -1,3 +1,4 @@
+from thirdweb.common.nft import upload_or_extract_uri
 from thirdweb.core.classes.contract_wrapper import ContractWrapper
 from thirdweb.core.classes.erc_721 import ERC721
 from thirdweb.abi import TokenERC721
@@ -5,6 +6,7 @@ from thirdweb.abi import TokenERC721
 from eth_account.account import LocalAccount
 from web3.eth import TxReceipt
 from web3 import Web3
+from thirdweb.core.classes.ipfs_storage import IpfsStorage
 from thirdweb.types.nft import NFTMetadata
 
 from thirdweb.types.sdk import SDKOptions
@@ -16,27 +18,32 @@ class NFTCollection(ERC721):
         self,
         provider: Web3,
         address: str,
+        storage: IpfsStorage,
         signer: Optional[LocalAccount],
         options: SDKOptions = SDKOptions(),
     ):
         abi = TokenERC721(provider, address)
         contract_wrapper = ContractWrapper(abi, provider, signer, options)
-        super().__init__(contract_wrapper)
+        super().__init__(contract_wrapper, storage)
 
     """
     WRITE FUNCTIONS
     """
 
     def mint(self, metadata: Union[NFTMetadata, str]) -> TxReceipt:
-        pass
+        return self.mint_to(self._contract_wrapper.get_signer_address(), metadata)
 
     def mint_to(self, to: str, metadata: Union[NFTMetadata, str]) -> TxReceipt:
-        pass
+        uri = upload_or_extract_uri(metadata, self._storage)
+        return self._contract_wrapper.send_transaction("mint_to", [to, uri])
 
     def mint_batch(self, metadatas: List[Union[NFTMetadata, str]]) -> TxReceipt:
-        pass
+        return self.mint_batch_to(
+            self._contract_wrapper.get_signer_address(), metadatas
+        )
 
     def mint_batch_to(
         self, to: str, metadatas: List[Union[NFTMetadata, str]]
     ) -> TxReceipt:
-        pass
+        # TODO: Implement - relies on MULTICALL
+        raise NotImplementedError
