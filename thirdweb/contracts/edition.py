5c3d2611e523ca3f230b4cc55e61982fe33a4c9a
adam-maj <mr.adam.maj@gmail.com>
2022-03-28 18:55:56 -0400

    Add documentation for Edition

--- thirdweb/contracts/edition.py
+++ thirdweb/contracts/edition.py
@@ -28,11 +28,26 @@ class Edition(ERC1155):
         super().__init__(contract_wrapper, storage)
 
     def mint(self, metadata_with_supply: EditionMetadataInput) -> TxReceipt:
+        """
+        Mint a new NFT to the connected wallet
+
+        :param metadata_with_supply: EditionMetadataInput for the NFT to mint
+        :returns: transaction receipt of the mint
+        """
+
         return self.mint_to(
             self._contract_wrapper.get_signer_address(), metadata_with_supply
         )
 
     def mint_to(self, to: str, metadata_with_supply: EditionMetadataInput) -> TxReceipt:
+        """
+        Mint a new NFT to the specified wallet
+
+        :param to: wallet address to mint the NFT to
+        :param metadata_with_supply: EditionMetadataInput for the NFT to mint
+        :returns: transaction receipt of the mint
+        """
+
         uri = upload_or_extract_uri(metadata_with_supply.metadata, self._storage)
         return self._contract_wrapper.send_transaction(
             "mint_to", [to, int(MAX_INT, 16), uri, metadata_with_supply.supply]
@@ -41,6 +56,14 @@ class Edition(ERC1155):
     def mint_additional_supply(
         self, token_id: int, additional_supply: int
     ) -> TxReceipt:
+        """
+        Mint additional supply of a token to the connected wallet
+
+        :param token_id: token ID to mint additional supply of
+        :param additional_supply: additional supply to mint
+        :returns: transaction receipt of the mint
+        """
+
         return self.mint_additional_supply_to(
             self._contract_wrapper.get_signer_address(), token_id, additional_supply
         )
@@ -48,6 +71,15 @@ class Edition(ERC1155):
     def mint_additional_supply_to(
         self, to: str, token_id: int, additional_supply: int
     ) -> TxReceipt:
+        """
+        Mint additional supply of a token to the specified wallet
+
+        :param to: wallet address to mint additional supply to
+        :param token_id: token ID to mint additional supply of
+        :param additional_supply: additional supply to mint
+        :returns: transaction receipt of the mint
+        """
+
         metadata = self._get_token_metadata(token_id)
         return self._contract_wrapper.send_transaction(
             "mint_to", [to, token_id, metadata.uri, additional_supply]
@@ -56,6 +88,13 @@ class Edition(ERC1155):
     def mint_batch(
         self, metadatas_with_supply: List[EditionMetadataInput]
     ) -> TxReceipt:
+        """
+        Mint a batch of NFTs to the connected wallet
+
+        :param metadatas_with_supply: list of EditionMetadataInput for the NFTs to mint
+        :returns: transaction receipt of the mint
+        """
+
         return self.mint_batch_to(
             self._contract_wrapper.get_signer_address(), metadatas_with_supply
         )
@@ -63,5 +102,13 @@ class Edition(ERC1155):
     def mint_batch_to(
         self, to: str, metadatas_with_supply: List[EditionMetadataInput]
     ) -> TxReceipt:
+        """
+        Mint a batch of NFTs to the specified wallet
+
+        :param to: wallet address to mint the NFTs to
+        :param metadatas_with_supply: list of EditionMetadataInput for the NFTs to mint
+        :returns: transaction receipt of the mint
+        """
+
         # TODO: Implement - relies on MULTICALL
         raise NotImplementedError
--- thirdweb/core/classes/erc_1155.py
+++ thirdweb/core/classes/erc_1155.py
@@ -30,18 +30,44 @@ class ERC1155(BaseContract):
     """
 
     def get(self, token_id: int) -> EditionMetadata:
+        """
+        Get metadata for a token
+
+        :param token_id: token ID to check the metadata for
+        :return: Metadata for the token
+        """
         supply = self._get_abi().total_supply.call(token_id)
         metadata = self._get_token_metadata(token_id)
         return EditionMetadata(metadata, supply)
 
     def get_all(self, query_params: QueryAllParams) -> List[EditionMetadata]:
+        """
+        Get the metadata for all tokens on the contract
+
+        :param query_params: optional QueryAllParams to define which tokens to get metadata for
+        :return: list of metadata for all tokens
+        """
+
         max_id = min(query_params.start + query_params.count, self.get_total_count())
         return [self.get(token_id) for token_id in range(query_params.start, max_id)]
 
     def get_total_count(self) -> int:
+        """
+        Get the total number of NFTs on the contract
+
+        :return: total number of tokens on the contract
+        """
+
         return self._get_abi().next_token_id_to_mint.call()
 
     def get_owned(self, address: str = "") -> List[EditionMetadataOwner]:
+        """
+        Get the metadata for all the tokens owned by an address
+
+        :param address: address to get the owned tokens for
+        :return: list of metadata for all tokens owned by the address
+        """
+
         owner = address if address else self._contract_wrapper.get_signer_address()
         max_id = self._get_abi().next_token_id_to_mint.call()
         balances = self._get_abi().balance_of_batch.call(
@@ -59,19 +85,54 @@ class ERC1155(BaseContract):
         return metadatas
 
     def total_supply(self, token_id: int) -> int:
+        """
+        Get the total number of tokens on the contract
+
+        :return: total number of tokens on the contract
+        """
+
         return self._get_abi().total_supply.call(token_id)
 
     def balance(self, token_id: int) -> int:
+        """
+        Get the connected wallets balance of a specific token
+
+        :param token_id: token ID to check the balance for
+        :return: balance of the token
+        """
+
         return self.balance_of(self._contract_wrapper.get_signer_address(), token_id)
 
     def balance_of(self, address: str, token_id: int) -> int:
+        """
+        Get a specific wallets balance of a specific token
+
+        :param address: address to check the balance for
+        :param token_id: token ID to check the balance for
+        :return: balance of the token
+        """
+
         return self._get_abi().balance_of.call(address, token_id)
 
     def is_transfer_restricted(self) -> bool:
+        """
+        Check if the contract is restricted so transfers can only be made by admins
+
+        :return: True if the contract is restricted, False otherwise
+        """
+
         # TODO: Implement - Relies on ROLES
         raise NotImplementedError
 
     def is_approved(self, address: str, operator: str) -> bool:
+        """
+        Check if an operator address is approved to manage a target addresses assets
+
+        :param address: address whose assets to check the approval of
+        :param operator: operator address to check the approval for
+        :return: True if the operator is approved, False otherwise
+        """
+
         return self._get_abi().is_approved_for_all.call(address, operator)
 
     """
@@ -81,18 +142,41 @@ class ERC1155(BaseContract):
     def transfer(
         self, to: str, token_id: int, amount: int, data: Any = [0]
     ) -> TxReceipt:
+        """
+        Transfer a specified token from the connected wallet to a specified address.
+
+        :param to: wallet address to transfer the tokens to
+        :param token_id: the specific token ID to transfer
+        :param amount: the amount of tokens to transfer
+        :returns: transaction receipt of the transfer
+        """
+
         fr = self._contract_wrapper.get_signer_address()
         return self._contract_wrapper.send_transaction(
             "safe_transfer_from", [fr, to, token_id, amount, data]
         )
 
     def burn(self, token_id: int, amount: int) -> TxReceipt:
+        """
+        Burn a specified amount of tokens from the connected wallet.
+
+        :param amount: amount of tokens to burn
+        :returns: transaction receipt of the burn
+        """
+
         account = self._contract_wrapper.get_signer_address()
         return self._contract_wrapper.send_transaction(
             "burn", [account, token_id, amount]
         )
 
     def set_approval_for_all(self, operator: str, approved: bool) -> TxReceipt:
+        """
+        Set the approval for an operator address to manage the connected wallets assets
+
+        :param operator: operator address to set the approval for
+        :param approved: True if the operator is approved, False otherwise
+        """
+
         return self._contract_wrapper.send_transaction(
             "set_approval_for_all", [operator, approved]
         )
