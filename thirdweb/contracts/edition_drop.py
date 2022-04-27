7eb17a5b85f444bdc369d55f745d93147640a1a3
adam-maj <mr.adam.maj@gmail.com>
2022-04-27 02:21:17 -0400

    Docs

--- thirdweb/contracts/edition_drop.py
+++ thirdweb/contracts/edition_drop.py
@@ -36,9 +36,9 @@ class EditionDrop(ERC1155[DropERC1155]):
     from eth_account import Account
     from web3 import Web3
 
-    // You can switch out this provider and RPC URL for your own
+    # You can switch out this provider and RPC URL for your own
     provider = Web3(Web3.HTTPProvider("<RPC_URL>"))
-    // This will create a random account to use for signing transactions
+    # This will create a random account to use for signing transactions
     signer = Account.create()
 
     sdk = ThirdwebSDK(provider, signer)
