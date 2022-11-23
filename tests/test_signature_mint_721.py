982ee30b8c8833e8b00a138741f5415e420fba5d
adam-maj <mr.adam.maj@gmail.com>
2022-11-23 13:31:23 -0500

    Require zero address in payload

--- tests/test_signature_mint_721.py
+++ tests/test_signature_mint_721.py
@@ -119,7 +119,6 @@ def test_claiming(
     assert nft_collection.get(tx[0].id).metadata.name == "OUCH VOUCH"
     assert nft_collection.balance_of(accounts[0].address) == 10
 
-
 @pytest.mark.usefixtures("sdk", "primary_account", "secondary_account")
 def test_custom_token(
     sdk: ThirdwebSDK,
