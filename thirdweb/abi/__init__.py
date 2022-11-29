61daaae5bd2601251d03ecf3d534471fdb0d2408
Adam Majmudar <64697628+adam-maj@users.noreply.github.com>
2022-11-29 17:19:24 -0500

    am/custom (#115)
    Add

--- thirdweb/abi/__init__.py
+++ thirdweb/abi/__init__.py
@@ -1,22 +1,20 @@
-from .t_w_factory import TWFactory
-from .t_w_registry import TWRegistry
-from .token_erc721 import TokenERC721
-from .token_erc1155 import TokenERC1155
-from .token_erc20 import TokenERC20
-from .marketplace import Marketplace
-from .multiwrap import Multiwrap
-from .ierc20 import IERC20
-from .ierc165 import IERC165
-from .ierc721 import IERC721
-from .ierc1155 import IERC1155
-from .drop_erc721 import DropERC721
-from .drop_erc1155 import DropERC1155
-from .contract_metadata_registry import ContractMetadataRegistry
-from .thirdweb_contract import ThirdwebContract
-from .i_platform_fee import IPlatformFee
-from .i_primary_sale import IPrimarySale
-from .i_permissions_enumerable import IPermissionsEnumerable
-from .i_royalty import IRoyalty
-from .i_token_erc20 import ITokenERC20
-from .i_token_erc721 import ITokenERC721
-from .i_token_erc1155 import ITokenERC1155
+from .t_w_factory import TWFactory # pylint: disable=unused-import
+from .t_w_registry import TWRegistry # pylint: disable=unused-import
+from .token_erc721 import TokenERC721 # pylint: disable=unused-import
+from .token_erc1155 import TokenERC1155 # pylint: disable=unused-import
+from .token_erc20 import TokenERC20 # pylint: disable=unused-import
+from .marketplace import Marketplace # pylint: disable=unused-import
+from .multiwrap import Multiwrap # pylint: disable=unused-import
+from .ierc20 import IERC20 # pylint: disable=unused-import
+from .ierc165 import IERC165 # pylint: disable=unused-import
+from .ierc721 import IERC721 # pylint: disable=unused-import
+from .ierc1155 import IERC1155 # pylint: disable=unused-import
+from .drop_erc721 import DropERC721 # pylint: disable=unused-import
+from .drop_erc1155 import DropERC1155 # pylint: disable=unused-import
+from .i_platform_fee import IPlatformFee # pylint: disable=unused-import
+from .i_primary_sale import IPrimarySale # pylint: disable=unused-import
+from .i_permissions_enumerable import IPermissionsEnumerable # pylint: disable=unused-import
+from .i_royalty import IRoyalty # pylint: disable=unused-import
+from .i_token_erc20 import ITokenERC20 # pylint: disable=unused-import
+from .i_token_erc721 import ITokenERC721 # pylint: disable=unused-import
+from .i_token_erc1155 import ITokenERC1155 # pylint: disable=unused-import
