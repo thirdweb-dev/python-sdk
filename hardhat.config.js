deef9eedcdf2cdaa1322e0208ae048cca9f5a2c2
Joaquim Verges <joaquim.verges@gmail.com>
2023-03-12 13:48:53 -0700

    Broaden event parsing after mint calls (#118)
    * Broaden event parsing after mint calls
    
    * update mainnet rpc
    
    * try to fix build issue
    
    * disable novella for now

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,15 +1,14 @@
-
 // Do not modify the existing settings
 module.exports = {
-    networks: {
-        hardhat: {
-            forking: {
-                url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.SDK_ALCHEMY_KEY}`,
-            },
-            chainId: 1,
-            initialBaseFeePerGas: 0, // Allow 0 gas fees when testing
-            throwOnTransactionFailures: true, // Brownie expects transactions to throw on revert
-            throwOnCallFailures: true, // Brownie expects calls to throw on failure
-       }
-    }
-}
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://ethereum.rpc.thirdweb.com/39a3c037d7a88e6692c6681bccfd1f1cf36370324c4051a83acd0edcffb20708`,
+      },
+      chainId: 1,
+      initialBaseFeePerGas: 0, // Allow 0 gas fees when testing
+      throwOnTransactionFailures: true, // Brownie expects transactions to throw on revert
+      throwOnCallFailures: true, // Brownie expects calls to throw on failure
+    },
+  },
+};
