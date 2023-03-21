f918aba1cbc11fe2cdece80e2ab9e507f8f23c48
Adam Majmudar <adam@Adams-MacBook-Pro.local>
2023-03-21 14:36:39 -0700

    Fix standards with functions

--- thirdweb/core/classes/erc_20_standard.py
+++ thirdweb/core/classes/erc_20_standard.py
@@ -1,9 +1,17 @@
-from typing import Generic
+from typing import List
 from thirdweb.abi.token_erc20 import TokenERC20
-from thirdweb.core.classes.base_contract import BaseContract
 from thirdweb.core.classes.contract_wrapper import ContractWrapper
+from thirdweb.core.classes.base_contract import BaseContract
 from thirdweb.core.classes.erc_20 import ERC20
 from thirdweb.core.classes.ipfs_storage import IpfsStorage
+from thirdweb.types.currency import (
+    Currency,
+    CurrencyValue,
+    Price,
+    TokenAmount,
+)
+from web3.eth import TxReceipt
+
 
 class ERC20Standard(BaseContract[TokenERC20]):
     _storage: IpfsStorage
@@ -12,18 +20,224 @@ class ERC20Standard(BaseContract[TokenERC20]):
     def __init__(self, contract_wrapper: ContractWrapper, storage: IpfsStorage):
         super().__init__(contract_wrapper)
         self._storage = storage
-
         self._erc20 = ERC20(contract_wrapper, storage)
-        self.get = self._erc20.get
-        self.balance = self._erc20.balance
-        self.balance_of = self._erc20.balance_of
-        self.total_supply = self._erc20.total_supply
-        self.allowance = self._erc20.allowance
-        self.allowance_of = self._erc20.allowance_of
-        self.is_transfer_restricted = self._erc20.is_transfer_restricted
-        self.transfer = self._erc20.transfer
-        self.transfer_from = self._erc20.transfer_from
-        self.set_allowance = self._erc20.set_allowance
-        self.transfer_batch = self._erc20.transfer_batch
-        self.burn = self._erc20.burn
-        self.burn_from = self._erc20.burn_from
+
+    def get(self) -> Currency:
+        """
+        Get the token metadata including name, symbol, decimals, etc.
+
+        ```python
+        token = contract.get()
+        print(token)
+        ```
+
+        :returns: token metadata
+        """
+
+        return self._erc20.get()
+
+    def balance(self) -> CurrencyValue:
+        """
+        Get the token balance of the connected wallet.
+
+        ```python
+        balance = contract.balance()
+        print(balance)
+        ```
+
+        :returns: balance of the connected wallet
+        """
+
+        return self._erc20.balance()
+
+    def balance_of(self, address: str) -> CurrencyValue:
+        """
+        Get the balance of the specified wallet
+
+        ```python
+        address = "{{wallet_address}}"
+        balance = contract.balance_of(address)
+        print(balance)
+        ```
+
+        :param address: wallet address to check the balance of
+        :returns: balance of the specified wallet
+        """
+
+        return self._erc20.balance_of(address)
+
+    def total_supply(self) -> CurrencyValue:
+        """
+        Get the total minted supply of the token.
+
+        :returns: total minted supply of the token
+        """
+
+        return self._erc20.total_supply()
+
+    def allowance(self, spender: str) -> CurrencyValue:
+        """
+        Get a specific spenders allowance of this token for the connected wallet.
+
+        ```python
+        spender = "{{wallet_address}}"
+        allowance = contract.allowance(spender)
+        ```
+
+        :param spender: wallet address to check the allowance of
+        :returns: allowance of the connected wallet
+        """
+
+        return self._erc20.allowance(spender)
+
+    def allowance_of(self, owner: str, spender: str) -> CurrencyValue:
+        """
+        Get the allowance of the specified spender for a specified owner.
+
+        ```python
+        # Address of the wallet who owns the funds
+        address = "{{wallet_address}}"
+
+        # Address of the wallet to check the token allowance
+        spender = "0x..."
+
+        allowance = contract.allowance_of(address, spender)
+        print(allowance)
+        ```
+
+        :param owner: wallet address whose assets will be spent
+        :param spender: wallet address to check the allowance of
+        :returns: allowance of the specified spender for the specified owner
+        """
+
+        return self._erc20.allowance_of(owner, spender)
+
+    def is_transfer_restricted(self) -> bool:
+        """
+        Check whether transfer is restricted for tokens in this module.
+
+        :returns: True if transfer is restricted, False otherwise
+        """
+
+        return self._erc20.is_transfer_restricted()
+
+    """
+    WRITE FUNCTIONS
+    """
+
+    def transfer(self, to: str, amount: Price) -> TxReceipt:
+        """
+        Transfer a specified amount of tokens from the connected wallet to a specified address.
+
+        ```python
+        # Address to send tokens to
+        to = "0x...
+
+        # Amount of tokens to transfer
+        amount = 0.1
+
+        contract.transfer(to, amount)
+        ```
+
+        :param to: wallet address to transfer the tokens to
+        :param amount: amount of tokens to transfer
+        :returns: transaction receipt of the transfer
+        """
+
+        return self._erc20.transfer(to, amount)
+
+    def transfer_from(self, fr: str, to: str, amount: Price) -> TxReceipt:
+        """
+        Transfer a specified amount of tokens from one specified address to another.
+
+        ```python
+        # Address to send tokens from
+        fr = "{{wallet_address}}"
+
+        # Address to send tokens to
+        to = "0x..."
+
+        # Amount of tokens to transfer
+        amount = 0.1
+
+        contract.transfer_from(fr, to, amount)
+        ```
+
+        :param fr: wallet address to transfer the tokens from
+        :param to: wallet address to transfer the tokens to
+        :param amount: amount of tokens to transfer
+        :returns: transaction receipt of the transfer
+        """
+
+        return self._erc20.transfer_from(fr, to, amount)
+
+    def set_allowance(self, spender: str, amount: Price) -> TxReceipt:
+        """
+        Sets the allowance of the specified wallet over the connected wallets funds to
+        a specified amount.
+
+        ```python
+        spender = "0x..."
+        amount = 100
+        contract.set_allowance(spender, amount)
+        ```
+
+        :param spender: wallet address to set the allowance of
+        :param amount: amount to set the allowance to
+        :returns: transaction receipt of the allowance set
+        """
+
+        return self._erc20.set_allowance(spender, amount)
+
+    def transfer_batch(self, args: List[TokenAmount]):
+        """
+        Transfer tokens from the connected wallet to many wallets.
+
+        ```python
+        from thirdweb.types.currency import TokenAmount
+
+        data = [
+            TokenAmount("{{wallet_address}}", 0.1),
+            TokenAmount("0x...", 0.2),
+        ]
+
+        contract.transfer_batch(data)
+        ```
+
+        :param args: list of token amounts and addressed to transfer to
+        :returns: transaction receipt of the transfers
+        """
+
+        return self._erc20.transfer_batch(args)
+
+    def burn(self, amount: Price) -> TxReceipt:
+        """
+        Burn a specified amount of tokens from the connected wallet.
+
+        ```python
+        amount = 0.1
+        contract.burn(amount)
+        ```
+
+        :param amount: amount of tokens to burn
+        :returns: transaction receipt of the burn
+        """
+
+        return self._erc20.burn(amount)
+
+    def burn_from(self, holder: str, amount: Price) -> TxReceipt:
+        """
+        Burn a specified amount of tokens from a specified wallet.
+
+        ```python
+        holder = "{{wallet_address}}"
+        amount = 0.1
+        contract.burn_from(holder, amount)
+        ```
+
+        :param holder: wallet address to burn the tokens from
+        :param amount: amount of tokens to burn
+        :returns: transaction receipt of the burn
+        """
+
+        return self._erc20.burn_from(holder, amount)
--- thirdweb/core/classes/erc_721_standard.py
+++ thirdweb/core/classes/erc_721_standard.py
@@ -1,9 +1,11 @@
-from typing import Generic
+from typing import Generic, List
 from thirdweb.core.classes.base_contract import BaseContract
 from thirdweb.core.classes.contract_wrapper import ContractWrapper
 from thirdweb.core.classes.erc_721 import ERC721
 from thirdweb.core.classes.ipfs_storage import IpfsStorage
 from thirdweb.types.contract import TERC721
+from thirdweb.types.nft import NFTMetadataOwner, QueryAllParams
+from web3.eth import TxReceipt
 
 
 class ERC721Standard(Generic[TERC721], BaseContract[TERC721]):
@@ -17,18 +19,169 @@ class ERC721Standard(Generic[TERC721], BaseContract[TERC721]):
     ):
         super().__init__(contract_wrapper)
         self._storage = storage
-
         self._erc721 = ERC721(contract_wrapper, storage)
-        self.get = self._erc721.get
-        self.get_all = self._erc721.get_all
-        self.get_total_count = self._erc721.get_total_count
-        self.owner_of = self._erc721.owner_of
-        self.total_supply = self._erc721.total_supply
-        self.balance = self._erc721.balance
-        self.balance_of = self._erc721.balance_of
-        self.is_transfer_restricted = self._erc721.is_transfer_restricted
-        self.is_approved = self._erc721.is_approved
-        self.transfer = self._erc721.transfer
-        self.burn = self._erc721.burn
-        self.set_approval_for_all = self._erc721.set_approval_for_all
-        self.set_approval_for_token = self._erc721.set_approval_for_token
+        
+    """
+    READ FUNCTIONS
+    """
+
+    def get(self, token_id: int) -> NFTMetadataOwner:
+        """
+        Get metadata for a token
+
+        ```python
+        nft = contract.get(0)
+        print(nft)
+        ```
+
+        :param token_id: token ID of the token to get the metadata for
+        :return: the metadata for the token and its owner
+        """
+
+        return self._erc721.get(token_id)
+
+    def get_all(
+        self, query_params: QueryAllParams = QueryAllParams()
+    ) -> List[NFTMetadataOwner]:
+        """
+        Get the metadata of all tokens in the contract
+
+        ```python
+        nfts = contract.get_all()
+        print(nfts)
+        ```
+
+        :param query_params: optionally define a QueryAllParams instance to narrow the metadata query to specific tokens
+        :return: the metadata of all tokens in the contract
+        """
+
+        return self._erc721.get_all(query_params)
+
+    def get_total_count(self) -> int:
+        """
+        Get the total number of NFTs minted by this contract
+
+        :return: the total number of NFTs minted by this contract
+        """
+
+        return self._erc721.get_total_count()
+
+    def owner_of(self, token_id: int) -> str:
+        """
+        Get the owner of a token
+
+        :param token_id: the token ID of the token to get the owner of
+        :return: the owner of the token
+        """
+        return self._erc721.owner_of(token_id)
+
+    def total_supply(
+        self,
+    ) -> int:
+        """
+        Get the total number of tokens in the contract
+
+        :return: the total number of tokens in the contract
+        """
+        return self._erc721.total_supply()
+
+    def balance(
+        self,
+    ) -> int:
+        """
+        Get the token balance of the connected wallet
+
+        :return: the token balance of the connected wallet
+        """
+
+        return self._erc721.balance()
+
+    def balance_of(self, address: str) -> int:
+        """
+        Get the token balance of a specific address
+
+        ```python
+        balance = contract.balance_of("{{wallet_address}}")
+        print(balance)
+        ```
+
+        :param address: the address to get the token balance of
+        """
+
+        return self._erc721.balance_of(address)
+
+    def is_transfer_restricted(
+        self,
+    ) -> bool:
+        """
+        Check if the contract is restricted to transfers only by admins
+
+        :return: True if the contract is restricted to transfers only by admins, False otherwise
+        """
+
+        return self._erc721.is_transfer_restricted()
+
+    def is_approved(self, address: str, operator: str) -> bool:
+        """
+        Check whether an operator address is approved for all operations of a specific addresses assets
+
+        :param address: the address whose assets are to be checked
+        :param operator: the address of the operator to check
+        :return: True if the operator is approved for all operations of the assets, False otherwise
+        """
+
+        return self._erc721.is_approved(address, operator)
+
+    """
+    WRITE FUNCTIONS
+    """
+
+    def transfer(self, to: str, token_id: int) -> TxReceipt:
+        """
+        Transfer a specified token from the connected wallet to a specified address.
+
+        ```python
+        to = "{{wallet_address}}"
+        token_id = 0
+
+        receipt = contract.transfer(to, token_id)
+        ```
+
+        :param to: wallet address to transfer the tokens to
+        :param token_id: the specific token ID to transfer
+        :returns: transaction receipt of the transfer
+        """
+
+        return self._erc721.transfer(to, token_id)
+
+    def burn(self, token_id: int) -> TxReceipt:
+        """
+        Burn a specified token from the connected wallet.
+
+        :param token_id: token ID of the token to burn
+        :returns: transaction receipt of the burn
+        """
+
+        return self._erc721.burn(token_id)
+
+    def set_approval_for_all(self, operator: str, approved: bool) -> TxReceipt:
+        """
+        Set the approval of an operator for all operations of a specific address's assets
+
+        :param operator: the address of the operator to set the approval for
+        :param approved: the address whos assets the operator is approved to manage
+        :returns: transaction receipt of the approval
+        """
+
+        return self._erc721.set_approval_for_all(operator, approved)
+
+    def set_approval_for_token(self, operator: str, token_id: int) -> TxReceipt:
+        """
+        Approve an operator for the NFT owner, which allows the operator to call transferFrom
+        or safeTransferFrom for the specified token.
+
+        :param operator: the address of the operator to set the approval for
+        :param token_id: the specific token ID to set the approval for
+        :returns: transaction receipt of the approval
+        """
+        return self._erc721.set_approval_for_token(operator, token_id)
